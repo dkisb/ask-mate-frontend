61b88779fc6064e5f0ce574e039c186490aa9297
dkisb <177304332+dkisb@users.noreply.github.com>
2025-04-09 11:19:42 +0200

    added routing

--- ask-mate-frontend/src/main.jsx
+++ ask-mate-frontend/src/main.jsx
@@ -3,12 +3,24 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import HomePage from './components/HomePage.jsx';
+import LoginPage from './components/LoginPage.jsx';
+import QuestionPage from './components/QuestionPage.jsx';
 import ErrorPage from './ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/home',
     element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/question/:id',
+    element: <QuestionPage />,
     errorElement: <ErrorPage />,
   },
 ]);
